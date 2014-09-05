b496d705bd6cec5c0f7bc63f3a031668687c4733
valek <valentinbochinski@gmail.com>
2014-09-05 13:44:05 +0200

    Ajout page Aide

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -24,4 +24,11 @@ RSpec.describe PagesController, :type => :controller do
     end
   end
 
+  describe "GET help" do
+    it "returns http success" do
+      get:help
+      expect(response).to be_success
+    end
+  end
+
 end
